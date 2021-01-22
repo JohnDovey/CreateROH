3b8b15181192aa72551c3b829fbaac6cffb54903
John Dovey <dovey.john@gmail.com>
2021-01-21 20:57:34 -0500

    First commit

---
+++ FrmMain.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CntEndRecord = New System.Windows.Forms.MaskedTextBox()
+        Me.CntStartRecord = New System.Windows.Forms.MaskedTextBox()
+        Me.BtnExtractData = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnFetchData = New System.Windows.Forms.Button()
+        Me.LblDateTime = New System.Windows.Forms.Label()
+        Me.LblFileName = New System.Windows.Forms.Label()
+        Me.RecordCounter = New System.Windows.Forms.NumericUpDown()
+        Me.TxtUrl = New System.Windows.Forms.TextBox()
+        Me.TxtLog = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.RecordCounter, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.AutoSize = True
+        Me.GroupBox1.Controls.Add(Me.CntEndRecord)
+        Me.GroupBox1.Controls.Add(Me.CntStartRecord)
+        Me.GroupBox1.Controls.Add(Me.BtnExtractData)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.BtnFetchData)
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(162, 425)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Menu"
+        '
+        'CntEndRecord
+        '
+        Me.CntEndRecord.AsciiOnly = True
+        Me.CntEndRecord.BeepOnError = True
+        Me.CntEndRecord.Culture = New System.Globalization.CultureInfo("en-US")
+        Me.CntEndRecord.Location = New System.Drawing.Point(6, 148)
+        Me.CntEndRecord.Mask = "00000"
+        Me.CntEndRecord.Name = "CntEndRecord"
+        Me.CntEndRecord.Size = New System.Drawing.Size(100, 20)
+        Me.CntEndRecord.TabIndex = 18
+        Me.CntEndRecord.Text = "0"
+        Me.CntEndRecord.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.CntEndRecord.ValidatingType = GetType(Integer)
+        '
+        'CntStartRecord
+        '
+        Me.CntStartRecord.AsciiOnly = True
+        Me.CntStartRecord.BeepOnError = True
+        Me.CntStartRecord.Culture = New System.Globalization.CultureInfo("en-US")
+        Me.CntStartRecord.Location = New System.Drawing.Point(6, 99)
+        Me.CntStartRecord.Mask = "00000"
+        Me.CntStartRecord.Name = "CntStartRecord"
+        Me.CntStartRecord.Size = New System.Drawing.Size(100, 20)
+        Me.CntStartRecord.TabIndex = 17
+        Me.CntStartRecord.Text = "0"
+        Me.CntStartRecord.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.CntStartRecord.ValidatingType = GetType(Integer)
+        '
+        'BtnExtractData
+        '
+        Me.BtnExtractData.Location = New System.Drawing.Point(6, 185)
+        Me.BtnExtractData.Name = "BtnExtractData"
+        Me.BtnExtractData.Size = New System.Drawing.Size(150, 23)
+        Me.BtnExtractData.TabIndex = 15
+        Me.BtnExtractData.Text = "Extract Data"
+        Me.BtnExtractData.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 132)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "End Record"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 83)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Start Record"
+        '
+        'BtnFetchData
+        '
+        Me.BtnFetchData.Location = New System.Drawing.Point(6, 48)
+        Me.BtnFetchData.Name = "BtnFetchData"
+        Me.BtnFetchData.Size = New System.Drawing.Size(150, 23)
+        Me.BtnFetchData.TabIndex = 6
+        Me.BtnFetchData.Text = "Fetch Data from Web"
+        Me.BtnFetchData.UseVisualStyleBackColor = True
+        '
+        'LblDateTime
+        '
+        Me.LblDateTime.AutoSize = True
+        Me.LblDateTime.Location = New System.Drawing.Point(184, 42)
+        Me.LblDateTime.Name = "LblDateTime"
+        Me.LblDateTime.Size = New System.Drawing.Size(22, 13)
+        Me.LblDateTime.TabIndex = 2
+        Me.LblDateTime.Text = "xxx"
+        Me.LblDateTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblFileName
+        '
+        Me.LblFileName.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LblFileName.AutoSize = True
+        Me.LblFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LblFileName.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.LblFileName.Location = New System.Drawing.Point(180, 22)
+        Me.LblFileName.Name = "LblFileName"
+        Me.LblFileName.Size = New System.Drawing.Size(53, 15)
+        Me.LblFileName.TabIndex = 3
+        Me.LblFileName.Text = "FileName"
+        Me.LblFileName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'RecordCounter
+        '
+        Me.RecordCounter.AutoSize = True
+        Me.RecordCounter.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RecordCounter.CausesValidation = False
+        Me.RecordCounter.Cursor = System.Windows.Forms.Cursors.No
+        Me.RecordCounter.Enabled = False
+        Me.RecordCounter.Location = New System.Drawing.Point(604, 12)
+        Me.RecordCounter.Maximum = New Decimal(New Integer() {100000, 0, 0, 0})
+        Me.RecordCounter.Name = "RecordCounter"
+        Me.RecordCounter.Size = New System.Drawing.Size(120, 16)
+        Me.RecordCounter.TabIndex = 4
+        Me.RecordCounter.ThousandsSeparator = True
+        '
+        'TxtUrl
+        '
+        Me.TxtUrl.Location = New System.Drawing.Point(180, 63)
+        Me.TxtUrl.Name = "TxtUrl"
+        Me.TxtUrl.Size = New System.Drawing.Size(608, 20)
+        Me.TxtUrl.TabIndex = 7
+        Me.TxtUrl.Text = "http://www.southafricawargraves.org/search/details.php?id="
+        '
+        'TxtLog
+        '
+        Me.TxtLog.AcceptsReturn = True
+        Me.TxtLog.Location = New System.Drawing.Point(187, 111)
+        Me.TxtLog.Multiline = True
+        Me.TxtLog.Name = "TxtLog"
+        Me.TxtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TxtLog.ShortcutsEnabled = False
+        Me.TxtLog.Size = New System.Drawing.Size(601, 326)
+        Me.TxtLog.TabIndex = 7
+        Me.TxtLog.TabStop = False
+        Me.TxtLog.Text = "Log Window"
+        Me.TxtLog.WordWrap = False
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TxtLog)
+        Me.Controls.Add(Me.TxtUrl)
+        Me.Controls.Add(Me.RecordCounter)
+        Me.Controls.Add(Me.LblFileName)
+        Me.Controls.Add(Me.LblDateTime)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "FrmMain"
+        Me.Text = "Create ROH from WarGraves Project"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.RecordCounter, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents LblDateTime As Label
+    Friend WithEvents LblFileName As Label
+    Friend WithEvents RecordCounter As NumericUpDown
+    Friend WithEvents BtnFetchData As Button
+    Friend WithEvents TxtUrl As TextBox
+    Friend WithEvents TxtLog As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnExtractData As Button
+    Friend WithEvents CntStartRecord As MaskedTextBox
+    Friend WithEvents CntEndRecord As MaskedTextBox
+End Class
---
+++ FrmMain.vb
@@ -0,0 +1,325 @@
+﻿Imports System.Data.SQLite
+Imports System.Net.Http
+Imports System.Net
+Imports System.IO
+Imports HtmlAgilityPack
+Imports System.Text
+
+Public Class FrmMain
+
+    Public StoreLog As String
+    Dim MySubDir As String
+    Dim MyDir As String
+    Dim DBName As String
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Cursor = Cursors.WaitCursor
+        LblDateTime.Text = DateAndTime.Now
+        MyDir = Application.StartupPath
+        DBName = Path.Combine(MyDir, "ROHData.Sql3")
+        LblFileName.Text = DBName
+        CheckDatabase(DBName)
+        MySubDir = Path.Combine(MyDir, "/pages/")
+        Directory.CreateDirectory(MySubDir)
+        Cursor = DefaultCursor
+
+    End Sub
+
+    Sub CheckDatabase(MyDbName)
+        Dim cs As String = "URI=file:" & MyDbName
+        Using con As New SQLiteConnection(cs)
+            con.Open()
+            Using cmd As New SQLiteCommand(con)
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS 'rawweb' ('id' INTEGER NOT NULL, 'StartTime'	TEXT COLLATE RTRIM,  'EndTime'	TEXT COLLATE RTRIM, 'PageSize'	NUMERIC DEFAULT 0, 'PersonNumber'	NUMERIC DEFAULT 0, 'WebAddress'	TEXT, 'WebPage'	TEXT, PRIMARY KEY('id' AUTOINCREMENT));"
+                cmd.ExecuteNonQuery()
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS 'LastSeq' ('id'	INTEGER NOT NULL DEFAULT 0, 'LastNumber'	NUMERIC NOT NULL DEFAULT 0, PRIMARY KEY('id'));"
+                cmd.ExecuteNonQuery()
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS 'Regiment'  ( 'RegimentID'	INTEGER NOT NULL UNIQUE, 'RegimentName'	TEXT, PRIMARY KEY('RegimentID'));"
+                cmd.ExecuteNonQuery()
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS 'Rank'  ( 'RankID'	INTEGER NOT NULL UNIQUE, 'RankName'	TEXT, 'RankDescription'	TEXT, PRIMARY KEY('RankID'));"
+                cmd.ExecuteNonQuery()
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS 'Unit'  ( 'UnitID'	INTEGER NOT NULL UNIQUE, 'UnitName'	TEXT, PRIMARY KEY('UnitID'));"
+                cmd.ExecuteNonQuery()
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS 'Country'  ( 'CountryID'	INTEGER NOT NULL UNIQUE, 'CountryName'	TEXT, PRIMARY KEY('CountryID'));"
+                cmd.ExecuteNonQuery()
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS 'Cemetery' ( 'CemeteryID'	INTEGER NOT NULL UNIQUE, 'CemeteryName'	TEXT, 'Lat'	TEXT, 'Long'	TEXT, PRIMARY KEY('CemeteryID'));"
+                cmd.ExecuteNonQuery()
+
+                cmd.CommandText = "CREATE TABLE  IF NOT EXISTS 'PersonInfoRaw' ( 'id'	INTEGER NOT NULL UNIQUE, 'PersonNumber'	INTEGER NOT NULL UNIQUE, 'Name'	TEXT NOT NULL DEFAULT 'Unknown', 'FirstName'	TEXT, 'LastName'	TEXT, 'Rank'	TEXT, 'RankID'	INTEGER, 'Regiment'	TEXT, 'RegimentID'	INTEGER, 'Unit'	TEXT, 'UnitID'	INTEGER, 'DateDeath'	TEXT DEFAULT 'Unknown', 'CauseDeath'	TEXT DEFAULT 'Unknown', 'AddInfo'	TEXT, 'Country'	TEXT, 'CountryID'	INTEGER, 'Cemetery'	INTEGER, 'CemeteryID'	INTEGER, 'CemeteryLat'	TEXT, 'CemeteryLong'	TEXT, 'GraveRef' TEXT, 'DateChecked'	TEXT, PRIMARY KEY('id')) ;"
+                cmd.ExecuteNonQuery()
+
+                'CONSTRAINT 'Regiment' FOREIGN KEY('RegimentID') REFERENCES Regiment), 
+                'CONSTRAINT 'Unit' FOREIGN KEY('UnitID') REFERENCES Unit), 
+                'CONSTRAINT 'Country' FOREIGN KEY('CountryID') REFERENCES Country), 
+                'Constraint 'Rank' FOREIGN KEY('RankID') REFERENCES Rank)
+
+                cmd.ExecuteNonQuery()
+                'cmd.CommandText = "INSERT INTO LastSeq VALUES (0,100);"
+                'cmd.ExecuteNonQuery()
+
+                cmd.CommandText = "SELECT LastNumber from LastSeq where ID = 0;"
+                RecordCounter.Value = cmd.ExecuteScalar()
+                CntStartRecord.Text = RecordCounter.Value.ToString
+
+                'Dim rdr As SQLiteDataReader = cmd.ExecuteReader()
+                'Using rdr
+                ' While (rdr.Read())
+                ' RecordCounter.Value = rdr("LastNumber")
+                ' CntStartRecord.Text = rdr("LastNumber") + 1
+                ' End While
+                'End Using
+
+            End Using
+            con.Close()
+
+
+        End Using
+    End Sub
+
+
+    Private Sub BtnFetchData_Click(sender As Object, e As EventArgs) Handles BtnFetchData.Click
+        ' Print a message as the page downloads.
+        TxtLog.Text = ""
+        For MyCount = Val(CntStartRecord.Text) To Val(CntEndRecord.Text)
+            TxtLog.AppendText("Downloading page: " & TxtUrl.Text & MyCount & vbCrLf)
+            DownloadPage(MyCount)
+            Application.DoEvents()
+        Next
+
+
+    End Sub
+
+    Public StartTime As String
+    Public EndTime As String
+    Public PageSize As Integer
+    Public WebAddress As String
+    Public WebPage As String
+    Public Response As String
+
+    Sub DownloadPage(MyPersNum)
+        Me.Cursor = Cursors.WaitCursor
+        RecordCounter.Value = MyPersNum
+
+        WebAddress = TxtUrl.Text & MyPersNum
+        StartTime = DateTime.Now
+        TxtLog.AppendText("Start: " & StartTime & vbCrLf)
+        LblDateTime.Text = StartTime
+        ' Download the page
+        Using Client As New WebClient
+            Client.Headers("User-Agent") = "Googlebot/2.38"
+            Response = Client.DownloadString(WebAddress)
+            'Dim arr() As Byte = client.DownloadData("http://www.dotnetperls.com/")
+            WebPage = Response
+        End Using
+        ' Finished downloading the page
+        EndTime = DateTime.Now
+        TxtLog.AppendText("End: " & EndTime & vbCrLf)
+        PageSize = WebPage.Length
+        TxtLog.AppendText(" Size: " & PageSize & vbCrLf)
+
+        LblDateTime.Text = EndTime
+        WriteData(MyPersNum, StartTime, EndTime, PageSize, WebAddress, WebPage)
+        Cursor = Cursors.Default
+    End Sub
+    Dim NewMyWebAddress As String
+    Dim NewMyWebPage As String
+
+
+
+    Sub WriteData(myPersonNumber, myStartTime, MyEndTime, myPageSize, myWebAddress, myWebPage)
+
+        NewMyWebAddress = WebUtility.HtmlEncode(myWebAddress)
+        NewMyWebPage = myPersonNumber & ".html"
+        NewMyWebPage = Path.Combine(MySubDir, NewMyWebPage)
+
+        ' File.WriteAllLines(NewMyWebPage, myWebPage)
+        File.WriteAllText(NewMyWebPage, myWebPage)
+
+        Dim cs As String = "URI=file:" & LblFileName.Text
+        Using con As New SQLiteConnection(cs)
+            con.Open()
+            Using cmd As New SQLiteCommand(con)
+                cmd.CommandText = "INSERT INTO rawweb VALUES (null, '" & myStartTime & "', '" & MyEndTime & "', " & myPageSize & ", " & myPersonNumber & ", '" & NewMyWebAddress & "', '" & NewMyWebPage & "');"
+                Debug.Print("NewMyWebPage: " & NewMyWebPage)
+                Debug.Print("SQL: " & cmd.CommandText)
+
+                cmd.ExecuteNonQuery()
+
+                cmd.CommandText = "UPDATE LastSeq SET LastNumber =" & myPersonNumber & " where id = 0;"
+                cmd.ExecuteNonQuery()
+
+            End Using
+            con.Close()
+        End Using
+    End Sub
+
+
+    Private Sub BtnExtractData_Click(sender As Object, e As EventArgs) Handles BtnExtractData.Click
+        TxtLog.Text = ""
+        Dim web As New HtmlWeb()
+        Dim URI As String
+        URI = Path.Combine(MySubDir, "2.html")
+        URI = Application.StartupPath & URI
+        Dim tmpText As String
+
+
+        Dim doc As HtmlDocument = web.Load(URI)
+
+        Dim cnt As Integer = 0
+        Dim highCnt As Integer = 0
+
+        ' Load array with all <td> elements
+        Dim FindCountry(50) As String
+        Console.Write("Find Country")
+        Dim htmlNodes = doc.DocumentNode.SelectNodes("//tr")
+        cnt = 0
+        For Each childnode As HtmlNode In htmlNodes.Descendants.Where(Function(n) n.Name = "td")
+            FindCountry(cnt) = childnode.InnerHtml
+            FindCountry(cnt) = Replace(FindCountry(cnt), Chr(9), Space(1))
+            FindCountry(cnt) = Replace(FindCountry(cnt), vbTab, Space(1))
+            FindCountry(cnt) = Trim(FindCountry(cnt))
+            tmpText = vbCrLf & "TD Element:" & cnt & "- >  " & FindCountry(cnt)
+            TxtLog.AppendText(tmpText)
+            Console.WriteLine(tmpText)
+            cnt += 1
+            highCnt = cnt
+        Next
+
+        Dim FindOne As Integer = -1
+        Dim FindTwo As Integer = -1
+        Dim FindThree As Integer = -1
+
+        Dim dbNamefld As String = ""
+        Dim dbFirstName As String = ""
+        Dim dbInitials As String = ""
+        Dim dbServiceNo As String = ""
+        Dim dbRank As String = ""
+        Dim dbRankID As String = ""
+        ' Regiment
+        Dim dbRegiment As String = ""
+        Dim dbRegimentID As String = ""
+        'Unit
+        Dim dbUnit As String = ""
+        Dim dbUnitID As String = ""
+        'Date of Death
+        Dim dbDateOfDeath As String = ""
+        Dim dbCauseOfDeath As String = ""
+        Dim dbAddInfo As String = ""
+        ' Country
+        Dim dbCountry As String = ""
+        Dim dbCountryID As String = ""
+        ' Cemetery
+        Dim dbCemetery As String = ""
+        Dim dbCemeteryID As String = ""
+        Dim dbGraveReference As String = ""
+
+        For cnt = 0 To (highCnt - 1)
+            Console.WriteLine("Cnt: " & cnt)
+            ' TD Element:0- >  <div class="tableLabel">Name:</div>
+            ' TD Element:  1- >  ABBEY
+            FindOne = FindCountry(cnt).IndexOf(">Name:")
+            If FindOne >= 0 Then
+                dbNamefld = FindCountry(cnt + 1) ' No change
+            End If
+
+            ' TD Element: 2- >  <div class="tableLabel">Given Name:</div>
+            ' TD Element: 3- >  DAVID ROBERT
+            FindOne = FindCountry(cnt).IndexOf(">Given Name:")
+            If FindOne >= 0 Then
+                dbFirstName = FindCountry(cnt + 1) ' No change
+            End If
+            ' TD Element: 4- >  <div class="tableLabel">Initials:</div>
+            ' TD Element: 5- >  D R
+            FindOne = FindCountry(cnt).IndexOf(">Initials:")
+            If FindOne >= 0 Then
+                dbInitials = FindCountry(cnt + 1) ' No change
+            End If
+            ' TD Element:6- >  <div class="tableLabel">Service No:</div>
+            ' TD Element: 7- >  7357
+            FindOne = FindCountry(cnt).IndexOf(">Service No:")
+            If FindOne >= 0 Then
+                dbServiceNo = FindCountry(cnt + 1) ' No change
+            End If
+            ' TD Element: 8- >  <div class="tableLabel">Rank:</div>
+            ' TD Element: 9- >  Private<div style='float:right;'>Other Casualties of this <a href='view-paginated.php?page=1&rank=429' target='new'>Rank</a></div>
+            'Rank=
+            ' dbRank = FindCountry(5) 'left up to <div
+            ' dbRankNo = FindCountry(5) ' rank=???' (5)
+            FindOne = FindCountry(cnt).IndexOf("Rank:")
+            If FindOne >= 0 Then
+                dbRank = FindCountry(cnt + 1)
+                dbRankID = FindCountry(cnt + 1)
+
+                FindTwo = dbRank.IndexOf("<div")
+                If FindTwo >= 0 Then
+                    dbRank = dbRank.Substring(0, FindTwo)
+                    Console.WriteLine("Rank: [{0}]", dbRank)
+                End If
+                FindTwo = dbRankID.IndexOf("rank=")
+                If FindTwo >= 0 Then
+                    FindThree = dbRankID.IndexOf("'", FindTwo)
+                    dbRankID = dbRankID.Substring(FindTwo + 5, FindThree - (FindTwo + 5))
+                End If
+                Console.WriteLine("RankID: [{0}]", dbRankID)
+            End If
+            ' TD Element: 10- >  <div class="tableLabel">Regiment:</div>
+            ' TD Element: 11- >  South African Infantry<div style='float:right;'>Other Casualties from this <a href='view-paginated.php?page=1&regiment=807' target='new'>Regiment</a></div>
+            FindOne = FindCountry(cnt).IndexOf(">Regiment:")
+            If FindOne >= 0 Then
+                dbRegiment = FindCountry(cnt + 1)
+                dbRegimentID = FindCountry(cnt + 1)
+
+                FindTwo = dbRegiment.IndexOf("<div")
+                If FindTwo >= 0 Then
+                    dbRegiment = dbRegiment.Substring(0, FindTwo)
+                    Console.WriteLine("Regiment: [{0}]", dbRegiment)
+                End If
+                FindTwo = dbRegimentID.IndexOf("regiment=")
+                If FindTwo >= 0 Then
+                    FindThree = dbRegimentID.IndexOf("'", FindTwo)
+                    dbRegimentID = dbRegimentID.Substring(FindTwo + 9, FindThree - (FindTwo + 9))
+                End If
+                Console.WriteLine("RegimentID: [{0}]", dbRegimentID
+
+            End If
+            'Unit
+            ' TD Element: 12- >  <div class="tableLabel">Unit:</div>
+            ' TD Element: 13- >  2nd Regt.<div style='float:right;'>Other Casualties from this <a href='view-paginated.php?page=1&unit=925' target='new'>Unit</a></div>
+            FindOne = FindCountry(cnt).IndexOf(">Unit:")
+            If FindOne >= 0 Then
+                dbUnit = FindCountry(cnt + 1)
+                dbUnitID = FindCountry(cnt + 1)
+
+                FindTwo = dbUnit.IndexOf("<div")
+                If FindTwo >= 0 Then
+                    dbUnit = dbUnit.Substring(0, FindTwo)
+                    Console.WriteLine("Unit: [{0}]", dbUnit)
+                End If
+                FindTwo = dbUnitID.IndexOf("Unit=")
+                If FindTwo >= 0 Then
+                    FindThree = dbUnitID.IndexOf("'", FindTwo)
+                    dbUnitID = dbUnitID.Substring(FindTwo + 5, FindThree - (FindTwo + 5))
+                End If
+                Console.WriteLine("UnitID: [{0}]", dbUnitID)
+            End If
+            'Date of Death
+
+            ' TD Element: 14- >  <div class="tableLabel">Date of Death:</div>
+            ' TD Element: 15- >  1916-12-20<div style='float:right;'>Other Casualties on this            <a href ='view-paginated.php?page=1&DoD_YYYY=1916&DoD_MM=12&DoD_DD=20' target='new'>Date</a></div>
+        Next cnt
+
+
+
+
+
+        Console.Write("End Find Country")
+
+
+
+    End Sub
+
+
+
+    Private Sub CntStartRecord_TextChanged(sender As Object, e As EventArgs) Handles CntStartRecord.TextChanged
+        CntEndRecord.Text = Val(CntStartRecord.Text) + 10
+    End Sub
+End Class
