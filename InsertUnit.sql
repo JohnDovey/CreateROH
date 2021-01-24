5278220d89e35e3674fc77ff18676ee1f692161f
John Dovey <dovey.john@gmail.com>
2021-01-24 08:55:31 -0500

    Added some SQL files

---
+++ InsertUnit.sql
@@ -0,0 +1,10 @@
+# Create Unit with Data from PersonInfoRaw
+BEGIN TRANSACTION; 
+Drop table Unit;
+CREATE TABLE IF NOT EXISTS 'Unit' (
+        'UnitID'    INTEGER NOT NULL UNIQUE,
+        'UnitName'  TEXT,
+        PRIMARY KEY('UnitID')
+);
+INSERT INTO Unit (UnitID, UnitName) SELECT UnitID, Unit FROM personinforaw GROUP BY Unitid;
+COMMIT; 
---
+++ insertRegiment.sql
@@ -0,0 +1,10 @@
+# Create Regiment with Data from PersonInfoRaw
+BEGIN TRANSACTION; 
+Drop table Regiment;
+CREATE TABLE IF NOT EXISTS 'Regiment' (
+        'RegimentID'    INTEGER NOT NULL UNIQUE,
+        'RegimentName'  TEXT,
+        PRIMARY KEY('RegimentID')
+);
+INSERT INTO Regiment (RegimentID, RegimentName) SELECT RegimentID, Regiment FROM personinforaw GROUP BY RegimentID;
+COMMIT; 
