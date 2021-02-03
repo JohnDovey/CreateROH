938f038c061ff6c06b349a9a7f3875f2b42a07a6
John Dovey <dovey.john@gmail.com>
2021-02-03 07:54:54 -0500

    Extracted CheckDatabase to a new program

---
+++ Mono2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mono2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
