3145c3863757a0e2f61838ff4bf38eba6e8ab2a9
John Dovey <dovey.john@gmail.com>
2021-02-04 15:28:37 -0500

    Update Charts

--- ROH/chartCauseOfDeath.php
+++ ROH/chartCauseOfDeath.php
@@ -45,13 +45,20 @@ require_once("include/menu.php");
                         <div class="col">
                             <div class="card">
                                 <div class="card-body">
-                                    <canvas id="YearPie" width="900"></canvas>
+                                    <canvas id="StatsGraph" width="900"></canvas>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <?php include_once('js/chartCauseOfDeath.php'); ?>
+                <?php 
+                $MyChartTitle = "Death by Cause Stats";
+                $MyChartType = "line";
+                if (isset($_GET['chart'])){
+                    $MyChartType = $_GET['chart'];
+                }
+                include_once('js/chartGeneric.php'); ?>
+<p><a href="<?=$_SERVER['PHP_SELF']?>?chart=bar" class="btn btn-primary" role="button">Bar Graph</a>|<a href="<?=$_SERVER['PHP_SELF']?>?chart=line" class="btn btn-primary" role="button">Line Graph</a>|<a href="<?=$_SERVER['PHP_SELF']?>?chart=radar" class="btn btn-primary" role="button">Radar Graph</a></p>
 
             </div> <!-- end Center Col -->
             <div class="col col-lg-2">
