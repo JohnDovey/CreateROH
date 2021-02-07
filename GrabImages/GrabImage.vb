f7aff27a49836d0c5c2e65d67f63c741f65007e1
John Dovey <dovey.john@gmail.com>
2021-02-06 23:06:29 -0500

    Added Project in correct place

---
+++ GrabImages/GrabImage.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.SQLite
+Imports System.Net.Http
+Imports System.Net
+Imports System.IO
+Imports System.Text
+Module GrabWebImages
+
+    Dim DBName As String
+    Sub Main()
+
+        DBName = Path.Combine(".", "ROHData.Sql3")
+        Dim cs As String = "URI=file:" & DBName
+        Using con As New SQLiteConnection(cs)
+            con.Open()
+            Using cmd As New SQLiteCommand(con)
+                cmd.CommandText = ""
+                cmd.ExecuteNonQuery()
+            End Using
+
+        End Using
+    End Sub
+
+End Module
