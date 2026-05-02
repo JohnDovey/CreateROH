a720ecb32200b6c1c189f373888df50cdfe27cc3
John Dovey <dovey.john@gmail.com>
2026-05-02 16:21:16 +0200

    Update bootstrap-footer.php
    ## Key Improvements 
    - Uses Bootstrap 5 native bundle (no separate jQuery + Popper needed)
    - Modern DOMContentLoaded + vanilla JS
    - Updated tooltip initialisation (data-bs-toggle)
    - Commented out Google Analytics (recommended for privacy)

--- ROH/include/bootstrap-footer.php
+++ ROH/include/bootstrap-footer.php
@@ -1,11 +1,44 @@
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<?php
+/**
+ * bootstrap-footer.php
+ * Footer scripts - Bootstrap 5 + jQuery minimal
+ */
+?>
 
-<!-- bootstrap -->
+<!-- Bootstrap 5 JS Bundle (includes Popper) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+        crossorigin="anonymous"></script>
+
+<!-- Font Awesome (already in head, but ensuring) -->
 <script src="https://kit.fontawesome.com/79aa4f4cb7.js" crossorigin="anonymous"></script>
+
+<!-- Custom JavaScript -->
 <script>
-$(function() {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+document.addEventListener('DOMContentLoaded', function() {
+    // Enable all tooltips
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function(tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    // Optional: Auto-dismiss alerts
+    const alerts = document.querySelectorAll('.alert');
+    alerts.forEach(function(alert) {
+        setTimeout(() => {
+            if (alert) alert.classList.add('fade');
+        }, 5000);
+    });
+});
 </script>
+
+<!-- Optional: Google Analytics (consider privacy - move to consent-based loading) -->
+<!-- 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CRBDFN0CZZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-CRBDFN0CZZ');
+</script>
+-->
