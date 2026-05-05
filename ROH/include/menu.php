fd06054b4d2818afa6e348ea63800fefbe30ea3f
John Dovey <dovey.john@gmail.com>
2026-05-05 15:02:48 +0200

    Update menu.php
    Added logo to menu

--- ROH/include/menu.php
+++ ROH/include/menu.php
@@ -1,98 +1,51 @@
 <?php
 /**
- * include/menu.php
- * Modern Bootstrap 5 Responsive Navigation
+ * include/menu.php - With Logo
  */
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow-sm">
     <div class="container-fluid">
-        <!-- Brand -->
-        <a class="navbar-brand fw-bold" href="index.php">
-            <i class="fa fa-cross"></i> Roll of Honour
+        <!-- Logo + Brand -->
+        <a class="navbar-brand d-flex align-items-center fw-bold" href="index.php">
+            <img src="images/For-the-Fallen.jpg" 
+                 alt="For the Fallen" 
+                 style="height: 48px; width: auto; border-radius: 6px; margin-right: 12px;">
+            <span>Roll of Honour</span>
         </a>
 
         <!-- Toggler -->
-        <button class="navbar-toggler" type="button" 
-                data-bs-toggle="collapse" 
-                data-bs-target="#navbarNav" 
-                aria-controls="navbarNav" 
-                aria-expanded="false" 
-                aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarNav" aria-controls="navbarNav" 
+                aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <!-- Menu Items -->
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php"><i class="fa fa-home"></i> Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="search.php"><i class="fa fa-search"></i> Search</a></li>
+                <li class="nav-item"><a class="nav-link" href="mainListPeople.php"><i class="fa fa-list"></i> Browse All</a></li>
 
-                <!-- Home -->
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">
-                        <i class="fa fa-home"></i> Home
-                    </a>
-                </li>
-
-                <!-- Search -->
-                <li class="nav-item">
-                    <a class="nav-link" href="search.php">
-                        <i class="fa fa-search"></i> Search
-                    </a>
-                </li>
-
-                <!-- Browse -->
-                <li class="nav-item">
-                    <a class="nav-link" href="mainListPeople.php">
-                        <i class="fa fa-list"></i> Browse All
-                    </a>
-                </li>
-
-                <!-- Lists Dropdown -->
+                <!-- Lists -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" 
-                       id="listsDropdown" role="button" 
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fa fa-table"></i> Lists
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="listsDropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Lists</a>
+                    <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="listPeopleRegiment.php">By Regiment</a></li>
-                        <li><a class="dropdown-item" href="listPeopleYear.php">By Year of Death</a></li>
+                        <li><a class="dropdown-item" href="listPeopleYear.php">By Year</a></li>
                         <li><a class="dropdown-item" href="listOnThisDay.php">On This Day</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="person.php">Single Person Lookup</a></li>
                     </ul>
                 </li>
 
-                <!-- Statistics Dropdown -->
+                <!-- Statistics -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" 
-                       id="statsDropdown" role="button" 
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fa fa-chart-bar"></i> Statistics
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="statsDropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Statistics</a>
+                    <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="chartYear.php">By Year</a></li>
-                        <li><a class="dropdown-item" href="chartAge.php">By Age</a></li>
                         <li><a class="dropdown-item" href="chartCauseOfDeath.php">Cause of Death</a></li>
-                        <li><a class="dropdown-item" href="chartRegiment.php">By Regiment</a></li>
                         <li><a class="dropdown-item" href="chartCountry.php">By Country</a></li>
-                        <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="pageviews.php">Page Views</a></li>
                     </ul>
                 </li>
-
-                <!-- Utilities -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" 
-                       id="utilsDropdown" role="button" 
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fa fa-tools"></i> Utilities
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="utilsDropdown">
-                        <li><a class="dropdown-item" href="DataInfo.php">Database Info</a></li>
-                        <li><a class="dropdown-item" href="search.php">Advanced Search</a></li>
-                    </ul>
-                </li>
-
             </ul>
         </div>
     </div>
